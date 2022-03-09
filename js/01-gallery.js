20f1867ff3e7fd25325f635124b3231e9ddd447f
AleksandrKolenov <kolenov.aa@yandex.ru>
2022-03-09 20:46:48 +0300

    JS 7 HELP

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,36 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+
+const galleryContainer = document.querySelector('.gallery');
+const cardsGallery = (createGalleryItems(galleryItems));
+galleryContainer.insertAdjacentHTML('beforebegin', cardsGallery);
+
+
+console.log(createGalleryItems(galleryItems));
+
+function createGalleryItems(galleryItems) {
+   return galleryItems.map(({ preview, original, description }) => {
+        return `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>;`
+    })
+    .join('');
+}
+
+galleryContainer.addEventListener('click', onClick);
+
+function onClick(evt) {
+  // if (evt.target.nodeName !== 'BUTTON') {
+  //   return;
+  // }
+  console.log(evt.target.textContent);
+}
+
